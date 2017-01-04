dce04329652158f1dd41c7e2c92e6ccb70edd871
Dave Toews <davejtoews@gmail.com>
2017-01-04 11:05:39 -0700

    Finish clients page, get time entry and project info from Freshbooks event

--- clients.php
+++ clients.php
@@ -5,33 +5,63 @@
 
 	require __DIR__ . '/vendor/autoload.php';
 
+	function printPageList($count) { ?>
+	
+		<ul>
+			<?php for ($i=1; $i<=$count; $i++) : ?>
+				<li>
+					<a href="?page=<?= $i ?>">Page <?= $i ?></a>
+				</li>
+			<?php endfor; ?>
+		</ul>
+
+	<?php }
+
+	function printClientList($clients) {?>
+
+		<ul>
+			<?php foreach ($clients as $client) : ?>
+				<li><?= $client['organization'] ?>: <?= $client['client_id'] ?></li>
+			<?php endforeach; ?>
+		</ul>
+
+	<?php }
+
 	use Noodlehaus\Config;
 
 	if(file_exists('config.json')) {
 		$conf = new Config('config.json');
 
 		if($conf->get('freshbooks.list_clients')) {
-			echo '<pre>';
 			$fb = new Freshbooks\FreshBooksApi($conf->get('freshbooks.domain'), $conf->get('freshbooks.token')); 
 		    
 		    	// Method names are the same as found on the freshbooks API
 			$fb->setMethod('client.list');
 
+			$page = 1;
+
+			if (isset($_GET['page'])) {
+				$page = $_GET['page'];
+			}
+
 			// For complete list of arguments see FreshBooks docs at http://developers.freshbooks.com
 			$fb->post(array(
-			    'per_page' => '100'
+			    'per_page' => '100',
+			    'page' => $page
 			));
 
 			$fb->request();
 
+			
 			if($fb->success()) {
-			    echo 'successful! the full response is in an array below';
-			    var_dump($fb->getResponse());
+			    $response = $fb->getResponse();
+			    printPageList($response['clients']['@attributes']['pages']);
+			    printClientList($response['clients']['client']);
 			} else {
 		 		echo $fb->getError();
 			    var_dump($fb->getResponse());
 			}
-	    	echo '</pre>';			
+	    				
 		} else {
 			echo "Set list_clients to true to list clients";
 		}
@@ -42,7 +72,4 @@
 		echo "Config.json not found";
 	}
 	
-
-	//print_r($_POST);
-    //file_put_contents("post.txt", print_r($_POST, true));
  ?>
--- index.php
+++ index.php
@@ -7,36 +7,85 @@
 
 	use Noodlehaus\Config;
 
-	if(file_exists('config.json')) {
+	function isClientProject($fb, $project_id) {
 		$conf = new Config('config.json');
 
-		echo '<pre>';
-		$fb = new Freshbooks\FreshBooksApi($conf->get('freshbooks.domain'), $conf->get('freshbooks.token')); 
-	    
-	    	// Method names are the same as found on the freshbooks API
-		$fb->setMethod('time_entry.get');
-
-		// For complete list of arguments see FreshBooks docs at http://developers.freshbooks.com
+		$fb->setMethod('project.get');
 		$fb->post(array(
-		    'time_entry_id' => '1207714'
+		    'project_id' => $project_id
 		));
 
 		$fb->request();
 
 		if($fb->success()) {
-		    echo 'successful! the full response is in an array below';
-		    var_dump($fb->getResponse());
+			$project = $fb->getResponse()['project'];
+			return true;
+			//if ($project['client_id'] == $conf->get('freshbooks.client_id')) {
+			//	return true;
+			//} else {
+			//	return false;
+			//}
 		} else {
-	 	echo $fb->getError();
+	 		echo $fb->getError();
+		    var_dump($fb->getResponse());
+		    return false;
+		}	
+	}
+
+	function handleFreshbooksEvent($name, $object_id) {
+		$conf = new Config('config.json');
+		$fb = new Freshbooks\FreshBooksApi($conf->get('freshbooks.domain'), $conf->get('freshbooks.token'));
+
+		$name_split = explode('.', $name);
+		$service = $name_split[0];
+		$action = $name_split[1];
+
+		switch ($service) {
+			case 'time_entry':
+				handleTimeEntryEvent($fb, $action, $object_id);
+				break;
+			default: 
+				return;
+		}
+
+	}
+
+	function handleTimeEntryEvent($fb, $action, $object_id) {
+
+	    // Method names are the same as found on the freshbooks API
+		$fb->setMethod('time_entry.get');
+
+		// For complete list of arguments see FreshBooks docs at http://developers.freshbooks.com
+		$fb->post(array(
+		    'time_entry_id' => $object_id
+		));
+
+		$fb->request();
+
+		if($fb->success()) {
+			$time_entry = $fb->getResponse()['time_entry'];
+			if (isClientProject($fb, $time_entry['project_id'])) {
+				echo "yep!!";
+			} else {
+				echo "nope211";
+			}
+		} else {
+	 		echo $fb->getError();
 		    var_dump($fb->getResponse());
 		}
-	    echo '</pre>';
+	}
+
+	if(file_exists('config.json')) {
+		echo "Config.json found\n";
+
+		if (isset($_POST['name']) && isset($_POST['object_id'])) {
+			echo "Freshbooks event submitted\n";
+			handleFreshbooksEvent($_POST['name'], $_POST['object_id']);
+		}
 
 	} else {
 		echo "Config.json not found";
 	}
 	
-
-	//print_r($_POST);
     //file_put_contents("post.txt", print_r($_POST, true));
  ?>
